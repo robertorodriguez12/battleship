d59ee9dd9c6c4c86ad781342ae01312ccd096236
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-09 14:14:53 -0600

    Create test for board has cells

---
+++ test/board_test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require 'minitest/nyan_cat'
+require './lib/board'
+require './lib/cell'
+require './lib/ship'
+require 'pry'
+
+class BoardTest < MiniTest::Test
+
+  def test_board_exists
+    board = Board.new
+
+    assert_instance_of Board, board
+
+  end
+
+  def test_board_has_cells
+    board = Board.new
+    board.generate_cells
+    expected = ['A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4', 'C1', 'C2', 'C3', 'C4', 'D1', 'D2', 'D3', 'D4']
+    assert_equal expected, board.cells.keys
+  end
+
+end
