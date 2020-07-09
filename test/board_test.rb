9949e86307325fa5780a43cd764da3f09b10c9d5
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-09 15:54:38 -0600

    Add test to validate placements

--- test/board_test.rb
+++ test/board_test.rb
@@ -19,4 +19,21 @@ class BoardTest < Minitest::Test
     assert_equal expected, board.cells.keys
   end
 
+  def test_board_has_valid_coordinates
+    board = Board.new
+     assert_equal true, board.valid_coordinate?("A1")
+     assert_equal false, board.valid_coordinate?("A5")
+     assert_equal false, board.valid_coordinate?("E1")
+     assert_equal false, board.valid_coordinate?("A22")
+  end
+
+  def test_board_can_validate_placements
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2"])
+    assert_equal false, board.valid_placement?(submarine, ["A2", "A3", "A4"])
+  end
+
 end
