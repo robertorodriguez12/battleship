f3495b4d0d25dd1e3015c31b8ecc054a41f776b9
Nell Smart <moonmargin@gmail.com>
2020-07-11 08:34:42 -0600

    Add to valid_placement? method

--- lib/board.rb
+++ lib/board.rb
@@ -11,6 +11,7 @@ class Board
   def initialize
     @cells = {}
     generate_cells
+    # binding.pry
   end
 
   def generate_cells
@@ -23,11 +24,15 @@ class Board
 
   def valid_coordinate?(key)
     @cells.has_key?(key)
-    binding.pry
   end
 
-  def valid_placement?(ship, coordinate)
-    coordinate.length == ship.length
+  def valid_placement?(ship, coordinate_array) # we check valid placement by arguing ship&attributes
+    # and the array of 2-3 coordinates
+    # ship length and coordinate_array length must be equal
+    return false if ship.length != coordinate_array.length
+    # Ship has to be either horizontally or vertically place, AND must not overlap with another ship
+    (valid_horizontal_placement?(coordinate_array) || valid_vertical_placement(coordinate_array)) &&
+    no_overlap?(coordinate_array)
+    # "return false if" statement - we don't use end?
   end
-
 end
