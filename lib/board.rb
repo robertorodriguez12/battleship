4c79866c41ca787b455f0ec14c5006c831a4294c
Nell Smart <moonmargin@gmail.com>
2020-07-11 10:00:07 -0600

    Add place method and pass overlap test

--- lib/board.rb
+++ lib/board.rb
@@ -97,4 +97,11 @@ class Board
     end
   end
 
+  def place(ship, coordinates)
+    if valid_placement?(ship, coordinates)
+      coordinates.each do |coordinate|
+        @cells[coordinate].place_ship(ship)
+    end
+      end
+  end
 end
