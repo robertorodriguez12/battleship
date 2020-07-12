d99bd73ca06ece92ad747ba215a1b14cdf493ee5
Nell Smart <moonmargin@gmail.com>
2020-07-11 18:23:36 -0600

    Add to both Cell and Board render methods

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,5 @@
 require 'pry'
 
-# ask instructors what "very little code outside of class" means for testing
-# very detailed testing - longer - especially for testing the hash
 class Board
   attr_reader :cells
 
@@ -11,7 +9,6 @@ class Board
   def initialize
     @cells = {}
     generate_cells
-    # binding.pry
   end
 
   def generate_cells
@@ -27,22 +24,13 @@ class Board
   end
 
   def valid_placement?(ship, coordinate_array)
-    # we check valid placement by arguing ship&attributes
-    # and the array of 2-3 coordinates
-    # ship length and coordinate_array length must be equal
     return false if ship.length != coordinate_array.length
-    # Ship has to be either horizontally or vertically placed, AND must not overlap with another ship
     (valid_horizontal_placement?(coordinate_array) || valid_vertical_placement?(coordinate_array)) &&
     no_overlap?(coordinate_array)
-    # "return false if" statement - we don't use end?
   end
 
   def valid_letters?(coordinates)
-    # iterate using map, store coordinates into letters variable
     letters = coordinates.map do |coordinate|
-      # block variable isolates each element  -as a string?
-      # split ex. "A4" into "A", "4"
-      # take first character and return .ord code point
       coordinate.split("").first.ord
     end
 
@@ -109,10 +97,9 @@ class Board
   def render(render_ship = false)
     row = ['A', 'B', 'C', 'D']
     row_index = 0
-    # add space before 1 below
     @cells.values.each_slice(4).reduce("  1 2 3 4") do |acc, cells|
       x = cells.map do |cell|
-        if render_ship
+        if render_ship == true
           "#{cell.render(true)} "
         else
           "#{cell.render} "
@@ -123,6 +110,5 @@ class Board
       row_index += 1
       acc
     end.concat("\n")
-
   end
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -98,14 +98,20 @@ class BoardTest < Minitest::Test
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
 
-    board.place(cruiser, ["A1", "A2", "A3"])
-
     expected1 = "  1 2 3 4 \n A . . . . \n B . . . . \n C . . . . \n D . . . . \n"
-
     assert_equal expected1, board.render
+
+    board.place(cruiser, ["A1", "A2", "A3"])
 
     expected2 = "  1 2 3 4 \n A S S S . \n B . . . . \n C . . . . \n D . . . . \n"
 
     assert_equal expected2, board.render(true)
   end
+
+  # def test_can_render_hits_misses_and_sunken_ships
+  #   board = Board.new
+  #   cruiser = Ship.new("Cruiser", 3)
+  #
+  #   board.place(cruiser, "A1", "A2", "A3")
+  # end
 end
