454ab6343bbb9f9a3fd1710fd940c4daf3649de3
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-11 18:14:11 -0600

    Add changes

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,4 @@
-
+#
 class Board
   attr_reader :cells
 
--- test/board_test.rb
+++ test/board_test.rb
@@ -1,5 +1,5 @@
 require 'minitest/autorun'
-require 'minitest/pride'
+require 'minitest/nyan_cat'
 require './lib/board'
 require './lib/cell'
 require './lib/ship'
@@ -7,69 +7,69 @@ require 'pry'
 
 class BoardTest < Minitest::Test
 
-  def test_board_exists
-    board = Board.new
+  # def test_board_exists
+  #   board = Board.new
+  #
+  #   assert_instance_of Board, board
+  # end
 
-    assert_instance_of Board, board
-  end
+  # def test_board_has_cells
+  #   board = Board.new
+  #   expected = [ 'A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4', 'C1', 'C2', 'C3', 'C4', 'D1', 'D2', 'D3', 'D4']
+  #   assert_equal expected, board.cells.keys
+  # end
 
-  def test_board_has_cells
-    board = Board.new
-    expected = [ 'A1', 'A2', 'A3', 'A4', 'B1', 'B2', 'B3', 'B4', 'C1', 'C2', 'C3', 'C4', 'D1', 'D2', 'D3', 'D4']
-    assert_equal expected, board.cells.keys
-  end
+  # def test_board_has_valid_coordinates
+  #   board = Board.new
+  #    assert_equal true, board.valid_coordinate?("A1")
+  #    assert_equal false, board.valid_coordinate?("A5")
+  #    assert_equal false, board.valid_coordinate?("E1")
+  #    assert_equal false, board.valid_coordinate?("A22")
+  # end
 
-  def test_board_has_valid_coordinates
-    board = Board.new
-     assert_equal true, board.valid_coordinate?("A1")
-     assert_equal false, board.valid_coordinate?("A5")
-     assert_equal false, board.valid_coordinate?("E1")
-     assert_equal false, board.valid_coordinate?("A22")
-  end
+  # def test_board_can_validate_placements
+  #   board = Board.new
+  #   cruiser = Ship.new("Cruiser", 3)
+  #   submarine = Ship.new("Submarine", 2)
+  #
+  #   assert_equal false, board.valid_placement?(cruiser, ["A1", "A2"])
+  #   assert_equal false, board.valid_placement?(submarine, ["A2", "A3", "A4"])
+  # end
 
-  def test_board_can_validate_placements
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
+  # def test_coordinates_are_consecutive
+  #   board = Board.new
+  #   cruiser = Ship.new("Cruiser", 3)
+  #   submarine = Ship.new("Submarine", 2)
+  #
+  #   assert_equal false, board.valid_placement?(cruiser, ["A1", "A2", "A4"])
+  #   assert_equal false, board.valid_placement?(submarine, ["A1", "C1"])
+  #   assert_equal false, board.valid_placement?(cruiser, ["A3", "A2", "A1"])
+  #   assert_equal false, board.valid_placement?(submarine, ["C1", "B1"])
+  #
+  # end
 
-    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2"])
-    assert_equal false, board.valid_placement?(submarine, ["A2", "A3", "A4"])
-  end
-
-  def test_coordinates_are_consecutive
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
-
-    assert_equal false, board.valid_placement?(cruiser, ["A1", "A2", "A4"])
-    assert_equal false, board.valid_placement?(submarine, ["A1", "C1"])
-    assert_equal false, board.valid_placement?(cruiser, ["A3", "A2", "A1"])
-    assert_equal false, board.valid_placement?(submarine, ["C1", "B1"])
-
-  end
-
-  def test_coordinates_are_not_diagonal
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-    submarine = Ship.new("Submarine", 2)
-
-    assert_equal false, board.valid_placement?(cruiser, ['A1', 'B2', 'C3'])
-  end
-
-  def test_ships_cannot_overlap
-   board = Board.new
-   cruiser = Ship.new("Cruiser", 3)
-
-   board.place(cruiser, ["A1", "A2", "A3"])
-
-   submarine = Ship.new("Submarine", 2)
-
-   board.valid_placement?(submarine, ["A1", "B1"])
-
-   assert_equal false, board.valid_placement?(submarine, ['A1', 'B1'])
-
-
-  end
+  # def test_coordinates_are_not_diagonal
+  #   board = Board.new
+  #   cruiser = Ship.new("Cruiser", 3)
+  #   submarine = Ship.new("Submarine", 2)
+  #
+  #   assert_equal false, board.valid_placement?(cruiser, ['A1', 'B2', 'C3'])
+  # end
+  #
+  # def test_ships_cannot_overlap
+  #  board = Board.new
+  #  cruiser = Ship.new("Cruiser", 3)
+  #
+  #  board.place(cruiser, ["A1", "A2", "A3"])
+  #
+  #  submarine = Ship.new("Submarine", 2)
+  #
+  #  board.valid_placement?(submarine, ["A1", "B1"])
+  #
+  #  assert_equal false, board.valid_placement?(submarine, ['A1', 'B1'])
+  #
+  #
+  # end
 
   def test_can_render_board
     # tests below are commented out because I cannot get the output to match the
@@ -81,7 +81,7 @@ class BoardTest < Minitest::Test
     board.place(cruiser, ["A1", "A2", "A3"])
 
     #### Board renders fine, the results just dont match up so cannot get test to pass
-    # 
+    # #
     # expected =  "  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n"
     #
     # assert_equal expected, board.render
@@ -89,16 +89,16 @@ class BoardTest < Minitest::Test
     expected2 = "  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n"
 
     assert_equal expected2, board.render(true)
-
+#
   end
 
-  def test_can_render_hits_misses_and_sunken_ships
-    board = Board.new
-    cruiser = Ship.new("Cruiser", 3)
-
-    board.place(cruiser, ["A1", "A2", "A3"])
-
-  end
+  # def test_can_render_hits_misses_and_sunken_ships
+  #   board = Board.new
+  #   cruiser = Ship.new("Cruiser", 3)
+  #
+  #   board.place(cruiser, ["A1", "A2", "A3"])
+  #
+  # end
 
 
 end
