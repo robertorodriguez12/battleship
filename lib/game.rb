ccc6a20e330f8bd1fb1887dbf9c8751cf769866a
Nell Smart <moonmargin@gmail.com>
2020-07-12 14:12:36 -0600

    Add plain English notes for Iteration 3 objectives

--- lib/game.rb
+++ lib/game.rb
@@ -36,3 +36,24 @@ class Game
 
 
 end
+
+# when firing a shot, display result to user
+# after user chooses to play, place both players' ships to set up
+# computer = random placement of ships (random element in keys array)
+# explanation of how to put ships in valid placements
+# invalid placement message to user if applicable
+
+# Single turn consists of:
+  # displaying boards
+  # player chooses coordinate to fire on
+  # computer chooses coodinate to fire on
+  # report results of player's shot
+  # report results of computer's shot
+      # and render board(s) accordingly
+
+# start of turn: user shown both boards but not computer's ships
+# user input: ask for a coordinate to fire upon
+# computer fires on a space but not one that's already fired upon
+# game over when both of one player's ships are sunk
+  # display message: You won, or I won!
+# return to main menu
