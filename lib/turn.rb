527ebc7274b05b9367b74f2d6dc81655c502c25e
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-13 13:19:34 -0600

    Begin defining turn class

---
+++ lib/turn.rb
@@ -0,0 +1,27 @@
+require 'pry'
+require './lib/board'
+require './lib/cell'
+require './lib/ship'
+
+class Turn
+
+  def initialize(coordinate)
+    @coordinate = coordinate
+
+  end
+
+  def ai_turn
+
+
+  end
+
+
+
+  def winner
+    return 
+
+
+  end
+
+
+end
